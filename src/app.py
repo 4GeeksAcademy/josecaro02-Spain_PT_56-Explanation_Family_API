7907e494102010366809c2e580fefa66bc2ea729
Jose David Caro Cantor <43614556+josecaro02@users.noreply.github.com>
2024-03-08 15:11:16 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -30,13 +30,22 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = request.get_json()
+    new_member = {
+            "id": jackson_family._generateId(),
+            "first_name": body["first_name"],
+            "last_name": jackson_family.last_name,
+            "age": body["age"],
+            "lucky_numbers": body["lucky_numbers"]
+        }
+    jackson_family.add_member(new_member)
+    print(body)
+    return jsonify("add member completed"), 200
 
-    return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
